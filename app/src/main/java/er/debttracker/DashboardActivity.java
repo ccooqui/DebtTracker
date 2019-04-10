839b4a21ac13768e82dad62f9e1d558b226bdd2d
Erica <eri8d@hotmail.com>
2019-04-10 17:35:51 -0400

    Added new debt activity.

--- app/src/main/java/er/debttracker/DashboardActivity.java
+++ app/src/main/java/er/debttracker/DashboardActivity.java
@@ -1,5 +1,6 @@
 package er.debttracker;
 
+import android.content.Intent;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -60,8 +61,7 @@ public class DashboardActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                    startActivity(new Intent(DashboardActivity.this, NewDebtActivity.class));
             }
         });
 
---
+++ app/src/main/java/er/debttracker/NewDebtActivity.java
@@ -0,0 +1,13 @@
+package er.debttracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewDebtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_debt);
+    }
+}
