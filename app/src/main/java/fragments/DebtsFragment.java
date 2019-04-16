112c4a11d1bc322dfbbf28c9dd39d478b1ee2ad0
jelinaramos <46576447+jelinaramos@users.noreply.github.com>
2019-04-15 23:13:10 -0400

    Added recycler View

--- app/src/main/java/fragments/DebtsFragment.java
+++ app/src/main/java/fragments/DebtsFragment.java
@@ -1,34 +1,115 @@
 package fragments;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.hardware.SensorManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import er.debttracker.R;
+import objects.Debts;
 
 
 public class DebtsFragment extends Fragment {
 
+    @SuppressLint("StaticFieldLeak")
+    private static List<Debts> debtsList = new ArrayList<>();
+    private RecycleViewAdapter recycleViewAdapter;
+    View v;
+
+    // Database retrieval
+    private FirebaseAuth fAuth;
+    private DatabaseReference mDatabase;
 
     public DebtsFragment() {
         // Required empty public constructor
     }
 
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        debtsList.clear();
+
+        if(v == null) {
+            v = inflater.inflate(R.layout.fragment_debts, container, false);
+
+            setHasOptionsMenu(true);
+
+            fAuth=FirebaseAuth.getInstance();
+            mDatabase = FirebaseDatabase.getInstance().getReference().child("Debts").child(fAuth.getCurrentUser().getUid());
+            mDatabase.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    // Retrieving debts from Firebase to display in RecycleView
+                    for(DataSnapshot debtsSnapshot : dataSnapshot.getChildren()){
+                        Debts debt = debtsSnapshot.getValue(Debts.class);
+                        debtsList.add(debt);
+                    }
+
+
+                    // Display nothing
+                    if(debtsList.size() == 0){
+                        Toast.makeText(getContext(), "No debts in the list. Go to the add debts tab to add some debts to your list!", Toast.LENGTH_LONG).show();
+                    } else {
+                        getRecycleView();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) { }
+            });
         }
 
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_debts, container, false);
+        return v;
     }
 
+    public void getRecycleView(){
+        // Creating a view of the fragment_debts .xml layout.
+        // Generates the icons and information taken from the debts list and displays each item
+        // in 2 columns and then returns the view
+        RecyclerView myRecyclerView = v.findViewById(R.id.debts_recycler_view);
+        recycleViewAdapter = new RecycleViewAdapter(getContext(), debtsList);
+        myRecyclerView.setAdapter(recycleViewAdapter);
+        GridLayoutManager mLayoutManager = new GridLayoutManager(getActivity(), 2);
+        myRecyclerView.setLayoutManager(mLayoutManager);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
 }
---
+++ app/src/main/java/fragments/RecycleViewAdapter.java
@@ -0,0 +1,157 @@
+package fragments;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import er.debttracker.R;
+import objects.Debts;
+import objects.Users;
+
+public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.myViewHolder>{
+    // View & Context
+    private View v;
+    private Context mContext;
+
+    // Data
+    private List<Debts> mData;
+    private FirebaseUser loggedInUserFB;
+
+    // Dialogs
+    private Dialog debtDialog;
+
+    // Accounts
+    private Users loggedInUser;
+
+
+    public RecycleViewAdapter() { }
+
+    public RecycleViewAdapter(Context mContext, List<Debts> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
+    @NonNull
+    @Override
+    public myViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        // For mini dialog that is displayed when a debt card item is clicked
+
+        // Setting up user account
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        loggedInUserFB = FirebaseAuth.getInstance().getCurrentUser();
+        assert loggedInUserFB != null;
+
+
+        v = LayoutInflater.from(mContext).inflate(R.layout.debt_cardview, viewGroup,false);
+        final myViewHolder viewHolder = new myViewHolder(v);
+
+        viewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                //Initialize the dialog
+                debtDialog = new Dialog(mContext);
+                debtDialog.setContentView(R.layout.debt_dialog);
+
+                debtDialog.show();
+
+                Button callButton = debtDialog.findViewById(R.id.call_dialog_btn);
+                callButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String phone = mData.get(viewHolder.getAdapterPosition()).getPhone();
+
+                        if(phone == null){
+                            Toast.makeText(mContext, "Phone number not available", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(mContext, "Make a call", Toast.LENGTH_SHORT).show();
+                            openCall(phone);
+                        }
+                    }
+                });
+
+                Button editButton = debtDialog.findViewById(R.id.edit_btn);
+                editButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+
+            }
+        });
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final myViewHolder myViewHolder, final int i) {
+
+            myViewHolder.tv_name.setText(mData.get(i).getDebtorName());
+            myViewHolder.tv_balance.setText(mData.get(i).getBalance());
+            myViewHolder.tv_dueDate.setText(mData.get(i).getFinalDueDate());
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    static class myViewHolder extends RecyclerView.ViewHolder{
+
+        // Gets the id names of all components to the cardView items
+        private TextView tv_name;
+        private TextView tv_balance;
+        private TextView tv_dueDate;
+
+        private CardView cardView;
+
+        myViewHolder(View v){
+            super(v);
+
+            tv_name = itemView.findViewById(R.id.tvDebtorName);
+            tv_balance = itemView.findViewById(R.id.tvBalance);
+            tv_dueDate =  itemView.findViewById(R.id.tvDueDate);
+            cardView = itemView.findViewById(R.id.id_Card);
+        }
+    }
+
+    private  void openCall(final String phone){
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+        mContext.startActivity(intent);
+    }
+
+
+}
+
